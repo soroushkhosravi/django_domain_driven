838ed6f45c8421bf38b2c98611649479ea5925b7
soroushkhosravi <soroushgol2022@gmail.com>
2023-10-16 04:42:21 +0100

    booksale and publisher tables are created.

--- src/data_models/models.py
+++ src/data_models/models.py
@@ -6,3 +6,20 @@ class Book(models.Model):
     class Meta:
         """Meta class for the model."""
         db_table = "book"
+
+class Publisher(models.Model):
+    class Meta:
+        """Meta class for the model."""
+        db_table = "publisher"
+    name = models.CharField(max_length=300)
+
+class BookSale(models.Model):
+    class Meta:
+        """Meta class for the model."""
+        db_table = "booksale"
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    pubdate = models.DateField()
